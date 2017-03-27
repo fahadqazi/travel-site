4ad244f45e47edff1d89914f36358962496b232a
Fahad Qazi <fahadqazi@gmail.com>
2017-03-27 23:40:02 +0100

    setting up gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,26 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    watch = require('gulp-watch');
+
+gulp.task('default', function(){
+  console.log("Gulp task running");
+})
+
+gulp.task('html', function(){
+  console.log("something happening to the index.html file")
+})
+
+gulp.task('styles', function(){
+  console.log("Imagine SASS or PostCSS tasks running.")
+})
+
+gulp.task('watch', function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  })
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  })
+
+})
